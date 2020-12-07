4bd07421f5a33e253930a7fe09ae4ce5ba625e6d
douglas04 <msantosbrioso@alu.ufc.br>
2020-12-07 02:52:17 +0000

    Passou em todos os testes

--- main.py
+++ main.py
@@ -4,7 +4,9 @@
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
     # COLOQUE SEU CÓDIGO AQUI
-
+print('digite um número:')
+num = input()
+print('o número informado foi {num}')
 
 if __name__ == '__main__':
     main()
